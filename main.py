14398a5bd4bcf0cdad33633c391acd9f7bed12d9
capo <capo@debian-BULLSEYE-live-builder-AMD64>
2023-05-15 02:48:08 +0300

    notes

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ MAX_LINES = 3
 MAX_BET = 1000
 MIN_BET = 10
 
-
+# reels in slot machine
 ROWS = 3
 COLS = 3
 
 # dictionary
+# symbols to choose in each reel
 symbol_count = {
     "A": 2,
     "B": 4,
@@ -21,7 +22,25 @@ symbol_count = {
 }
 
 
-# def get_slot_machine_spin():
+def get_slot_machine_spin(rows, cols, symbols):
+    # list of all symbols
+    all_symbols =[]
+    # iterate though the list dictionary to get values and their keys
+    for symbol, symbol_count in symbols.item():
+        # _ is an anonymous variable
+        for _ in range(symbol_count):
+            all_symbols.append(symbol) # e.g symbol A is added 2 times to the list
+
+    # select the values that go to each column
+    column = [] # will be a nested list [[], [], []]
+    # generate column values depending on the number of rows
+    for col in range(cols):
+        column = []
+        # remove selected value from all symbols list so that it is not selected again
+        # make a copy of the all symbols list
+        current_symbols = all_symbols[:] # the colon [:] is a slice operator
+        for row in range(rows):
+            value = random.choice(all_symbols)
 
 
 # collect user input
@@ -46,6 +65,7 @@ def deposit():
             print()
     return amount
 
+
 def get_number_of_lines():
     while True:
         lines = input("Enter the number of lines to bet on. (1 - " + str(MAX_LINES) + ")? ")
@@ -64,6 +84,7 @@ def get_number_of_lines():
             print("Please enter a number.")
             print()
     return lines
+
 
 def get_bet():
     while True:
